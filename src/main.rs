69b90eed6db3384210900f497812804e8f51405e
Dušan Andrić <Dusan.Andric@gmail.com>
2020-06-04 09:47:44 -0400

    Format and explicit typing on stream base url.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_files::Files;
-use dotenv::dotenv;
 use actix_web::client::Client;
-use std::{env};
-use std::net::{IpAddr};
+use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use dotenv::dotenv;
+use std::env;
+use std::net::IpAddr;
 use url::Url;
 
 async fn forward_video(
@@ -31,9 +31,7 @@ async fn forward_video(
 
     // Remove `Connection` as per
     // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection#Directives
-    for (header_name, header_value) in
-        res.headers().iter().filter(|(h, _)| *h != "connection")
-    {
+    for (header_name, header_value) in res.headers().iter().filter(|(h, _)| *h != "connection") {
         client_resp.header(header_name.clone(), header_value.clone());
     }
 
@@ -46,18 +44,17 @@ async fn main() -> std::io::Result<()> {
 
     let bind_addr: IpAddr = env::var("BIND_ADDR").unwrap().parse().unwrap();
     let bind_port = env::var("BIND_PORT").unwrap().parse::<u16>().unwrap();
-    let stream_base_url = env::var("STREAM_BASE_URL").unwrap().parse::<String>().unwrap();
-
+    let stream_base_url: String = env::var("STREAM_BASE_URL").unwrap().parse().unwrap();
     let forward_url = Url::parse(&stream_base_url).unwrap();
 
     HttpServer::new(move || {
         App::new()
-        .data(Client::new())
-        .data(forward_url.clone())
-        .wrap(middleware::Logger::default())
-        .service(web::resource("/video").route(web::get().to(forward_video)))
-        .service(Files::new("/images", "static/images/").show_files_listing())
-        .service(Files::new("/", "./static/root/").index_file("index.html"))
+            .data(Client::new())
+            .data(forward_url.clone())
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/video").route(web::get().to(forward_video)))
+            .service(Files::new("/images", "static/images/").show_files_listing())
+            .service(Files::new("/", "./static/root/").index_file("index.html"))
     })
     .bind((bind_addr, bind_port))?
     .system_exit()
@@ -66,4 +63,3 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
-
